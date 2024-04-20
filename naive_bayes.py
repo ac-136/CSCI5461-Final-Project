9ddabdb77d99ecb47a2d2089329bbd34b7fe0904
Jerry Yin <88324880+JerryYin777@users.noreply.github.com>
2024-04-20 16:06:26 -0500

    Naive_Bayes

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,26 +1,20 @@
-import random
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.preprocessing import LabelEncoder
+from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import LeaveOneOut, cross_val_score, cross_val_predict
 from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-# set seed
-random.seed(72)
-
-##### DATA #####
-# import data
-mrna_data = pd.read_csv('data/mRNA_RSEM_UQ_log2_Tumor.cct', delimiter="\t", index_col=0)
-clinical_data = pd.read_csv('data/clinical_table_140.tsv', delimiter="\t")
+# Load the data
+mrna_data = pd.read_csv('mRNA_RSEM_UQ_log2_Tumor.cct', sep='\t', index_col=0)
+clinical_data = pd.read_csv('clinical_table_140.tsv', sep='\t') 
 
 # arrange mrna_data as rows of features
 mrna_data = mrna_data.sort_index(axis=1) # sort by patient name
 mrna_data = mrna_data.T # transpose
 features = mrna_data.values # convert to np array
 
-# get cancer type for each patient
 patient_labels = clinical_data[['case_id', 'histology_diagnosis']].values
 patient_labels = patient_labels[patient_labels[:, 0].argsort()] # sort by patient name
 labels = patient_labels[:, 1] # just labels
@@ -28,8 +22,7 @@ labels = patient_labels[:, 1] # just labels
 encode = LabelEncoder()
 encoded_labels = encode.fit_transform(labels) # label encoding
 
-##### EVALUATE #####
-# TODO: define model
+model = GaussianNB(priors=None, var_smoothing=1e-09)
 
 # leave one out cross validation
 loo = LeaveOneOut()
@@ -58,3 +51,8 @@ plt.ylabel('True Labels')
 plt.title('Confusion Matrix')
 plt.show()
 
+# output
+# Mean Score: 0.9642857142857143
+# Accuracy: 0.9642857142857143
+# Precision: 0.9642857142857143
+# Recall: 1.0
