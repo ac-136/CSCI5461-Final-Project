07552711e824947df6936922615282e3018a6cb8
ac-136 <ashley.chen.mn@gmail.com>
2024-04-19 20:32:44 -0500

    set up data

---
+++ main.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+
+import random
+
+# set seed
+random.seed(72)
+
+##### DATA #####
+# import data
+mrna_data = pd.read_csv('data/mRNA_RSEM_UQ_log2_Tumor.cct', delimiter="\t", index_col=0)
+clinical_data = pd.read_csv('data/clinical_table_140.tsv', delimiter="\t")
+
+# arrange mrna_data as rows of features
+mrna_data = mrna_data.sort_index(axis=1) # sort by patient name
+mrna_data = mrna_data.T # transpose
+features = mrna_data.values # convert to np array
+
+# get cancer type for each patient
+patient_labels = clinical_data[['case_id', 'histology_diagnosis']].values
+patient_labels = patient_labels[patient_labels[:, 0].argsort()] # sort by patient name
+labels = patient_labels[:, 1] # just labels
+
+##### MODELS #####
+models = []
+names = []
+
+# define model
+
+# append model to models array
+
+# append model name to names array
+
+##### EVALUATE MODELS ######
