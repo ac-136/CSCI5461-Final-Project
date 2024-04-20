b116ae99de1ce94593cc2bff480b08fa4c6f88be
ac-136 <ashley.chen.mn@gmail.com>
2024-04-19 20:49:27 -0500

    framework for testing different models

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import random
 import numpy as np
 import pandas as pd
-
-import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import LeaveOneOut, cross_val_score, cross_val_predict
+from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix
 
 # set seed
 random.seed(72)
@@ -21,14 +25,36 @@ patient_labels = clinical_data[['case_id', 'histology_diagnosis']].values
 patient_labels = patient_labels[patient_labels[:, 0].argsort()] # sort by patient name
 labels = patient_labels[:, 1] # just labels
 
-##### MODELS #####
-models = []
-names = []
+encode = LabelEncoder()
+encoded_labels = encode.fit_transform(labels) # label encoding
 
-# define model
+##### EVALUATE #####
+# TODO: define model
 
-# append model to models array
+# leave one out cross validation
+loo = LeaveOneOut()
 
-# append model name to names array
+# evaluate model with mean score
+scores = cross_val_score(model, features, encoded_labels, cv = loo)
+mean_score = scores.mean()
+print("Mean Score:", mean_score)
 
-##### EVALUATE MODELS ######
+# other evaluation metrics
+pred = cross_val_predict(model, features, encoded_labels, cv=loo)
+accuracy = accuracy_score(encoded_labels, pred)
+precision = precision_score(encoded_labels, pred)
+recall = recall_score(encoded_labels, pred)
+
+print("Accuracy:", accuracy)
+print("Precision:", precision)
+print("Recall:", recall)
+
+# confusion matrix
+conf_matrix = confusion_matrix(encoded_labels, pred)
+plt.figure()
+sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues', cbar=False)
+plt.xlabel('Predicted Labels')
+plt.ylabel('True Labels')
+plt.title('Confusion Matrix')
+plt.show()
+
